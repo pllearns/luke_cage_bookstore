06bdc7ce1a4d76ffcd7a7da276dcf0aab77f9e4e
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-03-18 20:33:00 -0700

    Schema is complete. Will need to test with routing

--- fixture-data.sql
+++ fixture-data.sql
@@ -25,3 +25,85 @@ VALUES
     ('Ta-Nehisi Coates'),
     ('Huey P. Newton'),
     ('Bobby Seale');
+
+INSERT INTO
+  author_books
+SELECT
+  authors.id, books.id
+FROM
+  books
+CROSS JOIN
+  authors
+WHERE
+  books.title = 'Luke Cage Noir'
+AND
+  authors.name = 'Mike Benson';
+
+INSERT INTO
+  author_books
+SELECT
+  authors.id, books.id
+FROM
+  books
+CROSS JOIN
+  authors
+WHERE
+  books.title = 'The Autobiography of Malcolm X'
+AND
+  authors.name = 'Alex Haley';
+
+INSERT INTO
+  author_books
+SELECT
+  authors.id, books.id
+FROM
+  books
+CROSS JOIN
+  authors
+WHERE
+  books.title = 'The Beautiful Struggle'
+AND
+  authors.name = 'Ta-Nehisi Coates';
+
+INSERT INTO
+  book_genres
+SELECT
+  books.id, genres.id
+FROM
+  books
+CROSS JOIN
+  genres
+WHERE
+  books.title = 'To Die For The People'
+AND
+  genres.title = 'History';
+
+INSERT INTO
+  book_genres
+SELECT
+  books.id, genres.id
+FROM
+  books
+CROSS JOIN
+  genres
+WHERE
+  books.title = 'Luke Cage Noir'
+AND
+  genres.title = 'Comics';
+
+INSERT INTO
+  book_genres
+SELECT
+  books.id, genres.id
+FROM
+  books
+CROSS JOIN
+  genres
+WHERE
+  books.title = 'Seize The Time'
+AND
+  genres.title = 'History';
+
+SELECT now();
+
+SELECT * FROM author_books;
--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS genres;
 CREATE TABLE genres (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL
-)
+);
 
 DROP TABLE IF EXISTS book_genres;
 
@@ -32,3 +32,37 @@ CREATE TABLE author_books (
   author_id INTEGER NOT NULL,
   book_id INTEGER NOT NULL
 );
+
+SELECT now();
+
+SELECT * FROM books WHERE title LIKE '%something%';
+
+SELECT
+  books.*
+FROM
+  books
+LEFT JOIN
+  book_genres
+ON
+  books.id = book_genres.book_id
+LEFT JOIN
+  genres
+ON
+  genres.id = book_genres.genre_id
+WHERE
+  genres.title = 'Comics';
+
+SELECT
+  books.*
+FROM
+  books
+JOIN
+  author_books
+ON
+  books.id = author_books.book_id
+JOIN
+  authors
+ON
+  authors.id = author_books.author_id
+WHERE
+  authors.name = 'Alex Haley';
