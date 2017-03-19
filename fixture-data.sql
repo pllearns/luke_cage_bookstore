6c7534b5837e975d0e6f7454880bcbe0e09ce7f2
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-03-18 19:08:19 -0700

    WIP: finishing up schema files.

---
+++ fixture-data.sql
@@ -0,0 +1,27 @@
+SELECT now();
+
+INSERT INTO
+  books (title)
+VALUES
+  ('Luke Cage Noir'),
+  ('The Autobiography of Malcolm X'),
+  ('The Beautiful Struggle'),
+  ('To Die For The People'),
+  ('Seize The Time');
+
+INSERT INTO
+  genres (title)
+VALUES
+  ('Biography'),
+  ('History'),
+  ('Horror'),
+  ('Comics');
+
+INSERT INTO
+  authors (name)
+VALUES
+    ('Mike Benson'),
+    ('Alex Haley'),
+    ('Ta-Nehisi Coates'),
+    ('Huey P. Newton'),
+    ('Bobby Seale');
