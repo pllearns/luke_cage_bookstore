ce98991bcc582174ea6d23ed299688b26ca41b67
pllearns <pllearns@gmail.com>
2017-04-09 18:48:19 -0700

    Added initial chat bot service skeleton and made style changes to the shopping cart

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,40 @@ router.get('/', (request, response) =>  {
     page: page
   }
 
+// TODO: set up the chat service with IBM Watson
+
+  // var prompt = require('prompt-sync')()
+  // var ConversationV1 = require('watson-developer-cloud/conversation/v1')
+  //
+  // var conversation = new ConversationV1({
+  //   username: process.env.CHAT_BASE_USERNAME',
+  //   password: 'process.env.CHAT_BASE_PASSWORD',
+  //   path: {workspace: 'c673c7f3-3e82-4813-b9e3-eb912b33d1d0'},
+  //   version_date: '2016-07-11'
+  // })
+
+  // conversation.message({}, processResponse)
+  //
+  // function processResponse(err, response) {
+  //   if (err) {
+  //     console.error(err)
+  //     return
+  //   }
+  //
+  //   if (response.intents.length > 0) {
+  //     console.log('Detected intent: #' + response.intents[0].intent)
+  //   }
+  //
+  //   if (response.output.text != 0) {
+  //     console.log(response.output.text[0])
+  //   }
+  //
+  //   var newMessageFromUser = prompt('>> ')
+  //   conversation.message({
+  //     input: { text: newMessageFromUser }
+  //   }, processResponse)
+  // }
+
   database.searchForBooks(searchOptions)
     .then(books =>  {
       response.render('books/index', {
